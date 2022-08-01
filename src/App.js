0cda7fd1c7e2a8eae10374fc339267bb59183743
TheBigSteph <nzali.stephane86@gmail.com>
2022-08-01 23:19:19 +0100

    delete task

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react';
 import Form from './components/Form';
 import Items from './components/Items';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
   const [myState, setMyStat] = useState([
-    {task: 'Application avec react'},
-    {task: 'Faire du Sport'},
-    {task: 'Faire l\'amour'}
-  ])
+    {task: 'Application avec react', id: uuidv4()},
+    {task: 'Faire du Sport', id: uuidv4()},
+    {task: 'Faire l\'amour', id: uuidv4()}
+  ]);
+
+  const deleteTask = id => {
+    console.log(id)
+    const filteredData = myState.filter(item => {
+      return item.id !== id
+    })
+
+    setMyStat(filteredData)
+  }
 
   return (
     <div className="App">
       <div className="mx-auto px-4 col-12 col-sm-10 col-lg-6">
         <h1 className="text-center p-3">Todo List</h1>
         <Form />
-        {myState.map((item, index) => {
-          return <Items txt={item.task} />
+        {myState.map(item => {
+          return (
+            <Items 
+              txt={item.task} 
+              key={item.id}
+              id={item.id} 
+              func={deleteTask}
+            />
+          )
         })}
         
       </div>
